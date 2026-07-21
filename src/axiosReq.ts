292ff6e1d570c19477f0bc9b71dd4bf9e229a235
Musheeb <museebnoorisys240@gmail.com>
2026-07-21 23:13:44 +0530

    feat: packages

--- src/axiosReq.ts
+++ src/axiosReq.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
 
 interface User {
   name: string;
@@ -7,6 +8,39 @@ interface User {
   dob: string;
 }
 
-const fetchData: AxiosResponse<User> = async () => {
-  const response = await axios.get("https://dummyjson.com/test");
+const fetchData = async () => {
+  try {
+    const response: AxiosResponse<User> = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    console.log(response.data);
+    // const response = await axios.get("https://dummyjson.com/test");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("Error message: ", error.message);
+    }
+  }
 };
+
+function getData<T>(value: T): T {
+  return value;
+}
+
+const userData = getData<User>({
+  name: "Man",
+  age: 30,
+  isActive: true,
+  dob: "2000-07-01T00:00:001",
+});
+
+function useStateNumber(value: number) {
+  return value;
+}
+
+function useStateString(value: string) {
+  return value;
+}
+
+function useStateBoolean(value: boolean) {
+  return value;
+}
